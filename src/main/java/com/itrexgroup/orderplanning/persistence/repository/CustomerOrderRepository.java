d2a72062417c1f0e12bb737f9099433bb679ffb4
Pavel Tsekhanovich <contr.pavel.tsekhanovich@pandadoc.com>
2022-02-25 19:01:07 +0300

    clear test code

--- src/main/java/com/itrexgroup/orderplanning/persistence/repository/CustomerOrderRepository.java
+++ src/main/java/com/itrexgroup/orderplanning/persistence/repository/CustomerOrderRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
 
-
 }
--- src/main/java/com/itrexgroup/orderplanning/persistence/repository/CustomerRepository.java
+++ src/main/java/com/itrexgroup/orderplanning/persistence/repository/CustomerRepository.java
@@ -5,15 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-
-
-
-    /*select * from customers c
-join distances d on c.id = d.customer_id
-
-where c.id = 3 and d.warehouse_id in (select wp.warehouse_id from warehouse_product wp where wp.product_id = 1 )
-
-order by d.distance*/
-
-
 }
